d8199d0b1be17ee438e86f0be90b25797ba3e19a
Mahesh-74 <mahesh74.java@gmail.com>
2024-05-03 19:17:55 +0530

    FM

---
+++ src/main/java/com/krishna/prabha/mahe/eai1009employeemanagementrepo/Eai1009EmployeeManagementRepoApplication.java
@@ -0,0 +1,13 @@
+package com.krishna.prabha.mahe.eai1009employeemanagementrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eai1009EmployeeManagementRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Eai1009EmployeeManagementRepoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/krishna/prabha/mahe/eai1009employeemanagementrepo/Eai1009EmployeeManagementRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krishna.prabha.mahe.eai1009employeemanagementrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eai1009EmployeeManagementRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
